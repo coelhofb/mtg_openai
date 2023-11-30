13f7758f08f1296ac61660ddd17cf3a5a072ef34
fcoelho <fcoelho@Debian12.myguest.virtualbox.org>
2023-11-29 22:59:03 -0800

    Add all files

---
+++ app.py
@@ -0,0 +1,95 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+from flask import Flask, redirect, render_template, request, url_for
+from datetime import date, datetime, timezone
+from pymongo import MongoClient, ASCENDING, DESCENDING
+from bson.objectid import ObjectId
+from magic_open_ai import *
+
+
+
+MONGO_USER = os.getenv("MONGO_USER")
+MONGO_PASS = os.getenv("MONGO_PASS")
+OUT_IMG_PATH = 'static/cards/'
+MONGO_URL = 'mongodb://'+ MONGO_USER +':' + MONGO_PASS + '@localhost:27017'
+DB_MTG_CARDS = 'db_mtg_cards'
+DB_MTG_CARDS_COLLECTION = 'db_mtg_cards_collection'
+
+
+app = Flask(__name__)
+
+@app.route("/", methods=("GET", "POST"))
+def index():    
+    return render_template("index.html",blank=True)
+
+
+@app.route("/load", methods=("GET", "POST"))    
+def load_card():
+    mtg_card = {}
+    with MongoClient(MONGO_URL) as client:
+        db = client[DB_MTG_CARDS]
+        collection = db[DB_MTG_CARDS_COLLECTION] 
+        mtg_card = collection.find_one(sort=[( 'date', DESCENDING )])
+        client.close()
+  
+    return render_template("index.html", mtg_card=mtg_card, out_img_path=OUT_IMG_PATH)
+
+@app.route("/<card_id>", methods=("GET", "POST"))    
+def load_card_id(card_id):
+    mtg_card = {}
+    with MongoClient(MONGO_URL) as client:
+        db = client[DB_MTG_CARDS]
+        collection = db[DB_MTG_CARDS_COLLECTION]
+        mtg_card = collection.find({"_id": ObjectId(card_id)})[0]
+        client.close()
+  
+    return render_template("index.html", mtg_card=mtg_card, out_img_path=OUT_IMG_PATH)
+
+@app.route("/load_random_card", methods=("GET", "POST"))    
+def load_random_card():
+    mtg_card = {}
+    with MongoClient(MONGO_URL) as client:
+        db = client[DB_MTG_CARDS]
+        collection = db[DB_MTG_CARDS_COLLECTION]
+        sample = collection.aggregate([{ "$sample": { "size": 1 } }])
+        mtg_card = list(sample)[0]
+        client.close()
+  
+    return render_template("index.html", mtg_card=mtg_card , out_img_path=OUT_IMG_PATH)
+
+@app.route("/generate", methods=("GET", "POST"))    
+def generate_card():
+    received_post = request.form.to_dict()
+    theme = received_post["theme"]
+        
+    mtg_card = {}
+    mtg_card_img = ''
+    try:
+        mtg_card = generate_mtg_card(theme)
+        try:
+            mtg_card_img = generate_illustration(mtg_card["theme"], mtg_card["name"],OUT_IMG_PATH)
+        except:
+            mtg_card_img = 'error.png'    
+    except:
+        mtg_card = {"name": 'Error generating Mtg Card'}
+    
+    mtg_card["date"] = datetime.now(tz=timezone.utc)
+    mtg_card["illustration"] = mtg_card_img
+
+    #Store in Mongo DB
+    with MongoClient(MONGO_URL) as client:
+        db = client[DB_MTG_CARDS]
+        collection = db[DB_MTG_CARDS_COLLECTION]
+        collection.insert_one(mtg_card)
+
+    return render_template("index.html", mtg_card=mtg_card, out_img_path=OUT_IMG_PATH)
+
+@app.route("/generate_test", methods=("GET", "POST"))    
+def generate_card_test():
+    received_post = request.form.to_dict()
+    theme = received_post["theme"]
+
+    print(theme)
+    return render_template("index.html",blank=True)    
---
+++ magic_open_ai.py
@@ -0,0 +1,89 @@
+import os
+import openai
+import urllib.request
+import hashlib
+import numpy as np
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+GPT_MODEL = "gpt-3.5-turbo"
+GPT_MODEL_PARAMS = {"temperature": 1, "max_tokens": 256, "top_p":1, "frequency_penalty":0,"presence_penalty":0}
+
+def generate_mtg_card(theme):
+   query = """can you create 1 Magic the gathering card based on """ + theme + """ theme? The Mana cost needs to follow standard MTG rules.
+            Put the results in the following format:
+
+            Name: 
+            Mana Cost:
+            List of Different Colors: 
+            Type: 
+            Rarity: 
+            Power/Toughness: 
+            Ability:
+            """
+   openai.api_key = OPENAI_API_KEY
+   response = openai.ChatCompletion.create(
+    model=GPT_MODEL,
+    messages=[
+      {
+        "role": "user",
+        "content": query
+      },
+    ],
+    temperature=GPT_MODEL_PARAMS["temperature"],
+    max_tokens=GPT_MODEL_PARAMS["max_tokens"],
+    top_p=GPT_MODEL_PARAMS["top_p"],
+    frequency_penalty=GPT_MODEL_PARAMS["frequency_penalty"],
+    presence_penalty=GPT_MODEL_PARAMS["presence_penalty"]
+   )
+   
+   lines = response["choices"][0]["message"]["content"].splitlines()
+   lines_size = len(lines)
+   colors = lines[2].split(": ")[1].split(" ")
+   color_qty = 0
+   for color in colors:
+      if color not in ["", 'None']:
+         color_qty = color_qty +1	
+   mtg_card = {
+    "theme": theme
+    ,"name":lines[0].split(": ")[1]
+    ,"mana_cost":lines[1].split(": ")[1].replace("{","").replace("}","").replace("(","").replace(")","")
+    ,"color": colors
+    ,"color_qty": color_qty
+    ,"color_code" : ""
+    ,"type": lines[3].split(": ")[1]
+    ,"rarity":lines[4].split(": ")[1]
+    ,"pt":lines[5].split(": ")[1]
+    ,"ability":[]
+   }
+   if mtg_card["color_qty"] > 1:
+      mtg_card["color_code"] = 'Golden'
+   elif mtg_card["type"] == 'Land':
+      mtg_card["color_code"] = 'Land'
+   else: mtg_card["color_code"] = mtg_card["color"][0]
+
+   for i in range(6,lines_size):
+      ability = lines[i].split("Ability: ")[-1]
+      if ability:
+         mtg_card["ability"].append(ability)
+
+   return mtg_card
+
+def generate_illustration(theme, card_name,out_file_path):
+   openai.api_key = OPENAI_API_KEY
+   response = openai.Image.create(
+    prompt="a ilustration based on " + theme + " character " + card_name +", using Magic The Gathering style",
+    n=1,
+    size="512x512"
+   )
+   image_url = response['data'][0]['url']
+
+   salt = str(np.random.randint(100, size=1))
+   hashstr = hashlib.md5(str(image_url[-10:]+salt).encode()).hexdigest() # 32 character hexadecimal
+   mtg_img_file = hashstr[0:15]+'.png'
+   mtg_img_path = out_file_path+mtg_img_file
+
+   urllib.request.urlretrieve(image_url, filename=mtg_img_path)
+
+   return(mtg_img_file)
+
+
