b2c77deae7f59a48e4056564b91026b875179cca
fcoelho <fcoelho@Debian12.myguest.virtualbox.org>
2023-12-03 20:09:50 -0800

    bug fix

--- magic_open_ai.py
+++ magic_open_ai.py
@@ -52,11 +52,11 @@ def generate_mtg_card(theme):
       if color not in ["", 'Null', 'None', 'Colorless']:
          color_qty = color_qty +1	
    mtg_card["color_qty"] = color_qty
-
    if mtg_card["color_qty"] > 1:
     mtg_card["color_code"] = 'Golden'
    elif mtg_card["type"] == 'Land':
     mtg_card["color_code"] = 'Land'
+    mtg_card["mana_cost"] = ''
    elif mtg_card["color_qty"] == 0:
     mtg_card["color_code"] = 'Colorless'
    else: mtg_card["color_code"] = mtg_card["color"][0]
@@ -68,72 +68,10 @@ def generate_mtg_card(theme):
   
    return mtg_card
 
-def generate_mtg_card_bkp(theme):
-   query = """can you create 1 Magic the gathering card based on """ + theme + """ theme? The Mana cost needs to follow standard MTG rules.
-            Put the results in the following format:
-
-            Name: 
-            Mana Cost:
-            List of Different Colors: 
-            Type: 
-            Rarity: 
-            Power/Toughness: 
-            Ability:
-            """
-   openai.api_key = OPENAI_API_KEY
-   response = openai.ChatCompletion.create(
-    model=GPT_MODEL,
-    messages=[
-      {
-        "role": "user",
-        "content": query
-      },
-    ],
-    temperature=GPT_MODEL_PARAMS["temperature"],
-    max_tokens=GPT_MODEL_PARAMS["max_tokens"],
-    top_p=GPT_MODEL_PARAMS["top_p"],
-    frequency_penalty=GPT_MODEL_PARAMS["frequency_penalty"],
-    presence_penalty=GPT_MODEL_PARAMS["presence_penalty"]
-   )
-   
-   lines = response["choices"][0]["message"]["content"].splitlines()
-   lines_size = len(lines)
-   colors = lines[2].split(": ")[1].split(" ")
-   color_qty = 0
-   for color in colors:
-      if color not in ["", 'None']:
-         color_qty = color_qty +1	
-   mtg_card = {
-    "theme": theme
-    ,"name":lines[0].split(": ")[1]
-    ,"mana_cost":lines[1].split(": ")[1].replace("{","").replace("}","").replace("(","").replace(")","")
-    ,"color": colors
-    ,"color_qty": color_qty
-    ,"color_code" : ""
-    ,"type": lines[3].split(": ")[1]
-    ,"rarity":lines[4].split(": ")[1]
-    ,"pt":lines[5].split(": ")[1]
-    ,"ability":[]
-   }
-   if mtg_card["color_qty"] > 1:
-      mtg_card["color_code"] = 'Golden'
-   elif mtg_card["type"] == 'Land':
-      mtg_card["color_code"] = 'Land'
-   elif mtg_card["color_qty"] == 0:
-      mtg_card["color_code"] = 'Colorless'
-   else: mtg_card["color_code"] = mtg_card["color"][0]
-
-   for i in range(6,lines_size):
-      ability = lines[i].split("Ability: ")[-1]
-      if ability:
-         mtg_card["ability"].append(ability)
-
-   return mtg_card
-
-def generate_illustration_bkp(theme, card_name,card_type,out_file_path):
+def generate_illustration_dall2(theme, card_name,card_type,out_file_path):
    openai.api_key = OPENAI_API_KEY
    response = openai.Image.create(
-    prompt="Create an ilustration based on " + theme + " " + card_name + " " + card_type + '. The illustration should use Magic The Gathering artwork style.',
+    prompt="Create an ilustration based on " + theme + " " + card_name + " " + card_type + '. The illustration should use Magic The Gathering artwork illustration only style.',
     n=1,
     size="512x512"
    )
